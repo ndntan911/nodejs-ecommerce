ce3a86eafca6b017eb3dc2ff9e635ebddd21f1ad
ndntan911 <ndntan911@gmail.com>
2026-03-03 12:06:33 +0700

    setup project

---
+++ src/app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const helmet = require("helmet");
+const compression = require("compression");
+
+// init middlewares
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(compression());
+
+// init db
+
+// init routes
+app.get("/", (req, res) => {
+  const strCompress = "tan test";
+
+  return res
+    .status(200)
+    .json({ message: "Hello World!", metadata: strCompress.repeat(1000) });
+});
+
+// handle error
+
+module.exports = app;
