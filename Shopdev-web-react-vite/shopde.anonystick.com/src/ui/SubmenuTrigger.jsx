ef09f61474736de86e823eef6140c54c2665d1b3
ndntan911 <ndntan911@gmail.com>
2026-03-11 16:50:59 +0700

    remove fe

--- Shopdev-web-react-vite/shopde.anonystick.com/src/ui/SubmenuTrigger.jsx
+++
@@ -1,9 +0,0 @@
-const SubmenuTrigger = ({className, onClick}) => {
-    return (
-        <button className={className || ''} onClick={onClick} aria-label="Open submenu">
-            <i className="icon icon-ellipsis-vertical-solid text-[24px]" />
-        </button>
-    )
-}
-
-export default SubmenuTrigger
